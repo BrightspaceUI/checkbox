9f8bf94cdb56477cfec7adb600b7e832ee2df23b
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2018-07-04 19:32:35 -0400

    Linting and back to tabs

--- d2l-checkbox-spacer.js
+++ d2l-checkbox-spacer.js
@@ -16,24 +16,24 @@ const $_documentContainer = document.createElement('template');
 $_documentContainer.setAttribute('style', 'display: none;');
 
 $_documentContainer.innerHTML = `<dom-module id="d2l-checkbox-spacer">
-    <template strip-whitespace="">
-        <style>
-            :host {
-                display: block;
-                padding-left: 1.7rem;
-                margin-bottom: 0.9rem;
-            }
-            :host-context([dir="rtl"]) {
-                padding-right: 1.7rem;
-                padding-left: 0;
-            }
-        </style>
-        <slot></slot>
-    </template>
+	<template strip-whitespace="">
+		<style>
+			:host {
+				display: block;
+				padding-left: 1.7rem;
+				margin-bottom: 0.9rem;
+			}
+			:host-context([dir="rtl"]) {
+				padding-right: 1.7rem;
+				padding-left: 0;
+			}
+		</style>
+		<slot></slot>
+	</template>
 
 </dom-module>`;
 
 document.head.appendChild($_documentContainer.content);
 Polymer({
-    is: 'd2l-checkbox-spacer'
+	is: 'd2l-checkbox-spacer'
 });
