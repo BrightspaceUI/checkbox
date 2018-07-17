b750472e9f1583a429c510f5bbdd88bf40979cf6
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2018-07-17 17:14:15 -0400

    Cleanup

--- d2l-checkbox-spacer.js
+++ d2l-checkbox-spacer.js
@@ -4,11 +4,6 @@ Spacer to align secondary content with checkboxes
 
 @demo demo/index.html
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
 import '../@polymer/polymer/polymer-legacy.js';
 
 import { Polymer } from '../@polymer/polymer/lib/legacy/polymer-fn.js';
